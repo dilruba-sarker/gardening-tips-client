3cd55f499ad6e16e7f2ab1fecac08d1d35719825
dilruba-sarker <dilrubasarkerseo@gmail.com>
2025-05-20 01:29:34 +0600

    4th commit done

--- Root/Root.jsx
+++ Root/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../src/Components/Navbar/Navbar';
+import Footer from '../src/Components/Footer/Footer';
 
 const Root = () => {
     return (
         <div className='max-w-5xl mx-auto '>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
