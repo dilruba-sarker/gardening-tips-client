3a623cf5c15b5e4227b48ff831fd54d957b3f080
dilruba-sarker <dilrubasarkerseo@gmail.com>
2025-05-20 18:01:26 +0600

    8th commit

--- src/Pages/ShareAGardenTip/ShareAGardenTip.jsx
+++ src/Pages/ShareAGardenTip/ShareAGardenTip.jsx
@@ -1,32 +1,73 @@
 import React from 'react';
 
 const ShareAGardenTip = () => {
-    return (
-        <div className='text-center'>
-            <h1>Share AGarden Tip</h1>
-
-            <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col ">
+  return (
+    <div>
+<div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col">
     <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-     
+      <h1 className="text-2xl font-bold"> Add Garden Tips now!</h1>
+      
     </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+    <div className="card bg-base-100 w-full max-w-lg shrink-0 shadow-2xl">
       <div className="card-body">
         <form className="form">
+          <label className="label">Title</label>
+          <input type="text" name="title" className="input w-full"  placeholder="title" />
+          <label className="label">Plant Type</label>
+          <input type="text" name="plantType" className="input w-full"  placeholder="Plant Type" />
+
+          {/*  */}
+
+          <label className="label">Difficulty Level</label> <br /> 
+       <select defaultValue="Pick a color" className="select w-full">
+  <option disabled={true}>Pick a Level</option>
+  <option>Easy</option>
+  <option>Medium</option>
+  <option>Hard</option>
+</select>
+<br />
+
+ <label className="label">Description</label>
+          <input type="text" name="description" className="input w-full"  placeholder="Description" />
+ <label className="label">Images url</label>
+          <input type="text" name="Images" className="input w-full"  placeholder="Images url" />
+
+
+          {/*  */}
+           <label className="label">Category </label> <br /> 
+       <select defaultValue="Pick a color" className="select w-full">
+  <option disabled={true}>Pick a Category</option>
+  <option>Composting</option>
+  <option>Plant Care</option>
+  <option>Vertical Gardening</option>
+  <option>Fruit Gardening</option>
+  <option>Vegetable Gardening</option>
+</select>
+<br />
+           <label className="label">Availability </label> <br /> 
+       <select defaultValue="Pick Availability" className="select w-full">
+  <option disabled={true}>Pick a Category</option>
+  <option>Public</option>
+  <option> Hidden</option>
+  
+</select>
+<br />
+          {/*  */}
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
-          <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
-         
-          <button className="btn btn-neutral mt-4">Submit</button>
+          <input type=""  className="input w-full" value="email" placeholder="Email" />
+          <label className="label">Name</label>
+          <input type="text"  className="input w-full" value="name" placeholder="name" />
+        
+          
+          <div className='flex justify-center'><button className="btn btn-primary mt-4">Submit</button></div>
         </form>
       </div>
     </div>
   </div>
 </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default ShareAGardenTip;
